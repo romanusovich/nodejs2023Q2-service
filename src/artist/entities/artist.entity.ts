0930fcac47b98117c6bec2f151923027f651a0de
romanusovich1 <romanusovich1@gmail.com>
2023-07-31 11:19:22 +0300

    feat: add artist resource

---
+++ src/artist/entities/artist.entity.ts
@@ -0,0 +1,13 @@
+import { randomUUID } from "crypto";
+
+export class Artist {
+    id: string; // uuid v4
+    name: string;
+    grammy: boolean;
+
+    constructor(_name: string, _grammy: boolean) {
+        this.id = randomUUID();
+        this.name = _name;
+        this.grammy = _grammy;
+    }
+}
--- src/fake-db.ts
+++ src/fake-db.ts
@@ -1,9 +1,13 @@
+import { CreateArtistDto } from "./artist/dto/create-artist.dto";
+import { UpdateArtistDto } from "./artist/dto/update-artist.dto";
+import { Artist } from "./artist/entities/artist.entity";
 import { CreateUserDto } from "./user/dto/create-user.dto";
 import { UpdateUserDto } from "./user/dto/update-user.dto";
 import { User } from "./user/entities/user.entity";
 
 class FakeDB {
     users: User[] = [];
+    artists: Artist[] = [];
 
     createUser(createUserDto: CreateUserDto) {
         let newUser = new User(createUserDto.login, createUserDto.password);
@@ -23,7 +27,7 @@ class FakeDB {
     updateUser(id: string, updateUserDto: UpdateUserDto) {
         const user = this.users.filter((use) => use.id === id)[0];
         if (!user) return user;
-        user.password = updateUserDto.newPassword;
+        if (user.password === updateUserDto.oldPassword) user.password = updateUserDto.newPassword;
         return user;
     }
 
@@ -32,6 +36,36 @@ class FakeDB {
         if (!user) return user;
         this.users.splice(this.users.indexOf(user), 1);
         return user;
+    }
+
+    createArtist(createArtistDto: CreateArtistDto) {
+        let newArtist = new Artist(createArtistDto.name, createArtistDto.grammy);
+        this.artists.push(newArtist);
+        return newArtist;
+    }
+
+    findAllArtists() {
+        return this.artists;
+    }
+
+    findOneArtist(id: string) {
+        let artist = this.artists.filter((artis) => artis.id === id)[0];
+        return artist;
+    }
+
+    updateArtist(id: string, updateArtistDto: UpdateArtistDto) {
+        const artist = this.artists.filter((artis) => artis.id === id)[0];
+        if (!artist) return artist;
+        artist.name = updateArtistDto.name;
+        artist.grammy = updateArtistDto.grammy;
+        return artist;
+    }
+
+    removeArtist(id: string) {
+        const artist = this.artists.filter((artis) => artis.id === id)[0];
+        if (!artist) return artist;
+        this.artists.splice(this.artists.indexOf(artist), 1);
+        return artist;
     }
 }
 
