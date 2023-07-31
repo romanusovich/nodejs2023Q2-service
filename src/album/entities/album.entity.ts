ef80ba13b5eb6ad4353f4c6f123d45fa2bcd6052
romanusovich1 <romanusovich1@gmail.com>
2023-07-31 11:23:24 +0300

    feat: add album entity

--- src/album/entities/album.entity.ts
+++ src/album/entities/album.entity.ts
@@ -1,1 +1,15 @@
-export class Album {}
+import { randomUUID } from "crypto";
+
+export class Album {
+    id: string; // uuid v4
+    name: string;
+    year: number;
+    artistId: string | null; // refers to Artist
+
+    constructor(_name: string, _year: number, _artistId: string | null) {
+        this.id = randomUUID();
+        this.name = _name;
+        this.year = _year;
+        this.artistId = _artistId;
+    }
+}
