472c201f4a5198dd0bf5dfc0c27e005ffcfdaf87
romanusovich1 <romanusovich1@gmail.com>
2023-07-31 11:25:48 +0300

    feat: add album dto

--- src/album/dto/update-album.dto.ts
+++ src/album/dto/update-album.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAlbumDto } from './create-album.dto';
 
-export class UpdateAlbumDto extends PartialType(CreateAlbumDto) {}
+export class UpdateAlbumDto extends PartialType(CreateAlbumDto) {
+    name: string | null;
+    year: number | null;
+    artistId: string | null; // refers to Artist
+}
