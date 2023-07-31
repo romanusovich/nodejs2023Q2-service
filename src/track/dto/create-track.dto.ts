001c5354ed9d358000f7ec2c2c0bac8c3e27abaf
romanusovich1 <romanusovich1@gmail.com>
2023-07-31 11:49:40 +0300

    feat: add track dto

--- src/track/dto/create-track.dto.ts
+++ src/track/dto/create-track.dto.ts
@@ -1,1 +1,6 @@
-export class CreateTrackDto {}
+export class CreateTrackDto {
+    name: string;
+    artistId: string | null; // refers to Artist
+    albumId: string | null; // refers to Album
+    duration: number; // integer number
+  }
--- src/track/dto/update-track.dto.ts
+++ src/track/dto/update-track.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTrackDto } from './create-track.dto';
 
-export class UpdateTrackDto extends PartialType(CreateTrackDto) {}
+export class UpdateTrackDto extends PartialType(CreateTrackDto) {
+    name: string | null;
+    artistId: string | null; // refers to Artist
+    albumId: string | null; // refers to Album
+    duration: number | null; // integer number
+}
