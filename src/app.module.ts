b613545ceb76111beef6257d3155f3fd3018a2da
romanusovich1 <romanusovich1@gmail.com>
2023-07-31 11:44:27 +0300

    feat: add track resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
+import { TrackModule } from './track/track.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, ArtistModule, AlbumModule],
+  imports: [ConfigModule.forRoot(), UserModule, ArtistModule, AlbumModule, TrackModule],
   controllers: [AppController],
   providers: [AppService],
 })
