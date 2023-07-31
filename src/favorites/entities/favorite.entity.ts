7c0f04fc7eb1844301c6f20a9ce843c825ace805
romanusovich1 <romanusovich1@gmail.com>
2023-07-31 12:06:28 +0300

    feat: add favorite entity

--- src/favorites/entities/favorite.entity.ts
+++ src/favorites/entities/favorite.entity.ts
@@ -1,1 +1,11 @@
-export class Favorite {}
+export class Favorite {
+    artists: string[]; // favorite artists ids
+    albums: string[]; // favorite albums ids
+    tracks: string[]; // favorite tracks ids
+
+    constructor() {
+        this.artists = [];
+        this.albums = [];
+        this.tracks = [];
+    }
+}
